7da622a7f4664b5c2449fb00e53e596db428d093
qmatrix123 <qmatrix123@163.com>
2023-08-31 14:06:29 +0800

    初始化目录结构

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AuthLayout({ children }: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className='bg-red-500 h-full'>
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout
