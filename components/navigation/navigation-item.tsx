2c08aee3ef7c4ce7a72c16242506c37656d5de08
qmatrix123 <qmatrix123@163.com>
2023-09-01 17:59:54 +0800

    navigation sidebar 01

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Image from "next/image"
+import { useParams, useRouter } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+import { ActionTooltip } from "../action-tooltip"
+
+interface NavigationItemProps {
+    id: string
+    imageUrl: string
+    name: string
+}
+
+const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+    return (
+        <div>Server</div>
+    )
+}
+
+export default NavigationItem
