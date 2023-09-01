c1a5e3d938b668a59d880609b2161eaf9bbc5faa
qmatrix123 <qmatrix123@163.com>
2023-09-01 23:37:54 +0800

    Create Server Modal

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -3,30 +3,31 @@
 import { Plus } from 'lucide-react'
 import { ActionTooltip } from '../action-tooltip'
 import React from 'react'
+import { useModal } from '@/hooks/use-modal-store'
 
 const NavigationAction = () => {
-    return (
-        <div>
-            <ActionTooltip
-                side='right'
-                align='center'
-                label='Add a server'
-            >
-                <button
-                    className='group flex items-center'
-                >
-                    <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all
+  const { onOpen } = useModal()
+  return (
+    <div>
+      <ActionTooltip side="right" align="center" label="Add a server">
+        <button
+          onClick={() => onOpen('createServer')}
+          className="group flex items-center"
+        >
+          <div
+            className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all
                     overflow-hidden items-center justify-center bg-background dark:bg-enutral-700 group-hover:bg-emerald-500
-                ">
-                        <Plus
-                            className='group-hover:text-white transition text-emerald-500'
-                            size={25}
-                        />
-                    </div>
-                </button>
-            </ActionTooltip>
-        </div>
-    )
+                "
+          >
+            <Plus
+              className="group-hover:text-white transition text-emerald-500"
+              size={25}
+            />
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
+  )
 }
 
 export default NavigationAction
